b0135b98d62cdb8f753f2ffa0db9669310f1d1b9
sti <sti@matera.com>
2022-12-08 16:18:36 -0300

    Projeto inicial

---
+++ README.md
@@ -0,0 +1,18 @@
+### INFORMAÇÕES IMPORTANTES
+1. Node JS -> https://nodejs.org/en/
+2. node -v
+3. npm - v
+4. Yarn / React CLI ( https://classic.yarnpkg.com/en/ | https://create-react-app.dev/ )
+5. Instalando Yarn
+6. npm install -g yarn
+7. yarn -v
+
+-----------------// ESSE PROCESSO NÃO PRECISARÁ SER FEITO-------
+-> Instalando Create react App para gerar aplicações react.
+npm install -g create-react-app
+-----------------// ESSE PROCESSO NÃO PRECISARÁ SER FEITO-------
+
+### CRIANDO PROJETO E EXECUTANDO
+1. -----------------// PODEMOS UTILIZAR DESTE MODO COM NPX-------
+2. npx create-react-app front
+3. npm start / yarn start
---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+  state = {
+    numero1: '',
+    numero2: '',
+    resultado: ''
+  }
+
+  somar = () => {
+    const resultado = parseInt(this.state.numero1) + parseInt(this.state.numero2)
+    this.setState({resultado: resultado})
+  }
+
+  render() {
+    return(
+      <div>
+        <label>Primeoro Número:</label>
+        <input type="text" value={this.state.numero1} 
+              onChange={ (e) => this.setState({numero1: e.target.value}) } />
+
+        <br />
+        <label>Segundo Número:</label>
+        <input type="text" value={this.state.numero2} 
+              onChange={ (e) => this.setState({numero2: e.target.value}) } />
+
+        <br />
+        <button onClick={this.somar} >
+          Somar
+        </button>
+        
+        <br />
+        O resultado é: {this.state.resultado}
+      </div>
+    )
+  }
+}
+
+export default App;
